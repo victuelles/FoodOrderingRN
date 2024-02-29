57493cfa067aa706cc8043892ecbd09e526e2ae7
Rom Victuelles <rom.victuelles@gmail.com>
2024-02-29 13:33:03 -0800

    data added, first component

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,27 +1,42 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/src/components/EditScreenInfo';
+import { StyleSheet,Image } from 'react-native';
 import { Text, View } from '@/src/components/Themed';
+import Colors from '@/src/constants/Colors';
+import products from '../../../assets/data/products'
+
+const product =products[0]
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello World</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Image  style={styles.image} source={{uri: product.image}}/>
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor:'white',
+    padding:10,
+    borderRadius:10
+
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    paddingVertical:10
+  },
+  price: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: Colors.light.tint
+  },
+  image:{
+    width:'70%',
+    aspectRatio:1,
+  
   },
   separator: {
     marginVertical: 30,
